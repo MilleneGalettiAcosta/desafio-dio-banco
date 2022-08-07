154009bc59699e3032d116240cc0cd84f1cbd2f3
Millene Galetti <mikagaletti1@gmail.com>
2022-08-07 19:42:57 -0300

    Feat: Adicionando getter e setter - encapsulamento

--- src/ContaPoupança.java
+++ src/ContaPoupança.java
@@ -2,4 +2,16 @@ public class ContaPoupança {
     private int agencia;
     private int numeroConta;
     private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
