41f7e9b1e0571ddf26b9b53721331d5c131b0d43
Millene Galetti <mikagaletti1@gmail.com>
2022-08-07 20:07:47 -0300

    Feat: Criando contas e testando aplicação

--- src/ContaPoupança.java
+++ src/ContaPoupança.java
@@ -1,4 +1,12 @@
 
 public class ContaPoupança extends Conta{
 
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("Extrato conta poupança ===");
+        System.out.println(String.format("Agencia: %d", super.agencia));
+        System.out.println(String.format("Conta: %d", super.numeroConta));
+        System.out.println(String.format("Saldo: %2f", super.saldo));
+    }
+
 }
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Conta cc = new ContaCorrente();
+        Conta poupança = new ContaPoupança();
+
+        System.out.println(cc);
+        System.out.println(poupança);
+    }
+}
