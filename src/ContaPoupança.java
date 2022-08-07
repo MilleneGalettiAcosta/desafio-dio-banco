c2ae2249c4faf9e2fdf088b8783c158c3ab7726b
Millene Galetti <mikagaletti1@gmail.com>
2022-08-07 20:11:00 -0300

    Refactor: Criando método de imprimir informações

--- src/ContaPoupança.java
+++ src/ContaPoupança.java
@@ -4,9 +4,6 @@ public class ContaPoupança extends Conta{
     @Override
     public void imprimirExtrato() {
         System.out.println("Extrato conta poupança ===");
-        System.out.println(String.format("Agencia: %d", super.agencia));
-        System.out.println(String.format("Conta: %d", super.numeroConta));
-        System.out.println(String.format("Saldo: %2f", super.saldo));
+        super.imprimirInfos();
     }
-
 }
